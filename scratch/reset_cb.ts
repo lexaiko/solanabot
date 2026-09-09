d3f0cf9ca4c34c7d8645c4a27730344bf4778e6c
lexaiko <ekosusantobagussusanto@gmail.com>
2026-09-09 13:06:03 +0700

    chore: remove scratch directory from remote git tracking

--- scratch/reset_cb.ts
+++
@@ -1,4 +0,0 @@
-import { resetCircuitBreaker, isCircuitBreakerActive } from '../src/db/index';
-
-resetCircuitBreaker();
-console.log('Circuit Breaker status after reset:', isCircuitBreakerActive());
